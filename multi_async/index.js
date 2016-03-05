9a3fc6507ab0bed485e484838960fcd1af0ab22f
153330685 <153330685@qq.com>
2016-03-05 14:26:04 +0800

    多异步协同高并发

--- multi_async/index.js
+++ multi_async/index.js
@@ -1,24 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-var after = require('./multi_async.js');
+var dosomething = require('./multi_async.js');
 var PORT = process.env.PORT || 80;
 
 var server = http.createServer();
 server.on('request', function(req, res){
-	var done = after(3, function(result){
+	dosomething(function(result){
 		res.end(JSON.stringify(result));
-	});
-
-	fs.readFile('t1.txt', 'utf-8', function(err, data){
-		done('t1', data);
-	});
-
-	fs.readFile('t2.txt', 'utf-8', function(err, data){
-		done('t2', data);
-	});
-
-	fs.readFile('t3.txt', 'utf-8', function(err, data){
-		done('t3', data);
 	});
 });
 server.listen(PORT, function(){
--- multi_async/multi_async.js
+++ multi_async/multi_async.js
@@ -1,12 +1,44 @@
-function after(times, callback){
-	var count = 0, result = {};
-	return function(key, value){
-		result[key] = value;
-		count++;
-		if(count === times){
-			callback(result);
-		}
-	}
+var fs = require('fs');
+var events = require('events');
+var proxy = new events.EventEmitter();
+proxy.setMaxListeners(0);
+var count = 0;
+var status = 'ready';
+
+function after(times) {
+    var count = 0,
+        result = {};
+    return function(key, value) {
+        if (key) result[key] = value;
+        count++;
+        if (count === times) {
+            proxy.emit('done', result);
+            status = 'ready';
+        }
+    }
 }
 
-module.exports = after;
+function dosomething(done) {
+    fs.readFile('t1.txt', 'utf8', function(err, data) {
+        done('t1', data);
+    });
+
+    fs.readFile('t2.txt', 'utf8', function(err, data) {
+        done('t2', data);
+    });
+
+    fs.readFile('t3.txt', 'utf8', function(err, data) {
+        done('t3', data);
+    });
+}
+
+function doing(callback) {
+    proxy.once('done', callback);
+    if (status === 'ready') {
+        status = 'pending';
+        dosomething(after(3));
+        console.log(++count);
+    }
+}
+
+module.exports = doing;
